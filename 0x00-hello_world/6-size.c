b91b8615f3f5b263b95e68729531a63f5507d766
saber <sabertaj123@gmail.com>
2023-09-07 12:04:15 -0700

    0x00-hello_world mandatory

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+ *main-Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char is: %d byte(s)\n", sizeof(char));
+	printf("Size of an int is : %d byte(s)\n", sizeof(int));
+	printf("Size of a long int is: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int is: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float is: %d byte(s)\n", sizeof(float));
+	return (0);
+}
