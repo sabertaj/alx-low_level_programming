31d5fdcc26f6cfb68232e762e7fdd4272135466e
saber <sabertaj123@gmail.com>
2023-09-08 11:34:41 -0700

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,6 +18,6 @@ int main(void)
 	if (n < 0)
 		printf("%d is negative\n", n);
 	if (n == 0)
-		printf("%d is is zero\n", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,10 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
 	if (l > 5)
-		printf("last digit of %d is %d and is greater than 5\n", n, l);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	if (l == 0)
-		printf("last digit of %d is %d and is 0\n", n, l);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	if (l < 6 && l != 0)
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, l);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	return (0);
 }
